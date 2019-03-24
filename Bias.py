022e5eff95ab282305e119d7141abddd8544ad07
jjurss <47798142+jjurss@users.noreply.github.com>
2019-03-24 17:28:03 -0500

    Spelling

--- Bias.py
+++ Bias.py
@@ -226,7 +226,7 @@ class CodonAdaptationIndex(object):
 # example of how to use the code
 # create a new object of class CodonAdaptationIndex()
 test = CodonAdaptationIndex("abau.heg.fasta")
-#creat a CIA for which calculation is desired
+#create a CIA for which calculation is desired
 test.generate_rscu_index()
 test.generate_nrscu_index()
 #print the desired index
