f38a58742d9e879eb491ad0a0c61147e6a3e3259
Karson Lychuk <32942839+klychuk@users.noreply.github.com>
2019-04-07 14:54:47 -0500

    __main__ added

--- Unit_tests/testpipe.py
+++ Unit_tests/testpipe.py
@@ -4,4 +4,16 @@ import unittest
 class Test(unittest.Testcase):
 	
 	def test_get_bias(self, fasta):
+		result = ""
+		self.assertequal(result, get_bias(fasta)
 	
+	def test_get_data(self, fasta):
+		result = ""
+		self.assertequal(result, get_bias(fasta)
+	
+	def test_gethegs(self, fasta):
+		result = ""
+		self.assertequal(result, get_bias(fasta)
+	
+if __name__ == '__main__':
+	unittest.main()
