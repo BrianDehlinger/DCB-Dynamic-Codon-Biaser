cfbbfd2008bcaf42d75a79a1f1727ec70e009afd
Brian <bdehlinger@luc.edu>
2019-05-07 19:17:10 -0700

    Styling

--- testApp/app/__init__.py
+++ testApp/app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 
 ## This is a configuration file. UPLOAD_FOLDER specifies where user uploads will go. 
 ## The secret_key is necessary to run and should be hard to guess. 
 ## the MAX_CONTENT_LENGTH can be changed to change how large uploads can be. For example 18 * 1024 * 1024 will allow 18MB uploads. 
-UPLOAD_FOLDER = r"/home/bribri/DCB-Dynamic-Codon-Biaser/testApp/"
+UPLOAD_FOLDER = r"/home/bdehlinger/Stable_DCB/DCB-Dynamic-Codon-Bias/testApp/"
 app = Flask(__name__)
 app.secret_key = "dragon"
+bootstrap = Bootstrap(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
