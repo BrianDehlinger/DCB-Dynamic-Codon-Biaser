1007208d8189f7de9b6794230d582e0796131cbf
Brian <bdehlinger@luc.edu>
2019-05-13 16:09:41 -0700

    Refactor NCBIGet.py

--- testApp/NCBIGet.py
+++ testApp/NCBIGet.py
@@ -4,50 +4,68 @@ import re
 import os
 import subprocess
 
-## Gets the HTML file for the NCBI accession number of interest.
-def get_accession_data(accession):
-    the_request = requests.get("https://www.ncbi.nlm.nih.gov/nuccore/" + accession)
-    soup = bs4.BeautifulSoup(the_request.text)
+
 
 ## Base URL Of NCBI common to all URLS
-    temporaryURL = 'https://www.ncbi.nlm.nih.gov'
+the_request = None
+temporaryURL = 'https://www.ncbi.nlm.nih.gov'
+soup = None
+viewer_nucleotide = None
+viewer_soup = None
 
-## Function that finds first href that contain part of the urlPiece in the url. 
-    def find_url(urlPiece, soup):
-        try:
-            theURL = ''
-            for a in soup.find_all('a', href=True):
+## Function that finds first href that contain part of the urlPiece in the url. First the NCBI
+## sidebar is parsed to find an assembly link. If an assembly link is not present then the 
+## program will attempt to find the assembly link in the viewer. If however, the organsim is not a bacteria, then the program will raise an error. Similiary if there is no assembly link the program will also throw an error.
+
+def find_url(urlPiece, soup, viewer_soup):
+    try:
+        theURL = ''
+        if 'Bacteria' not in str(viewer_soup):
+            raise ValueError("The Nucleotide accession specified is not a Bacteria!")
+        for a in soup.find_all('a', href=True):
+            if('/assembly' in a['href']):
+                theURL = temporaryURL + a['href']
+                break
+        if theURL:
+            return theURL
+        else:
+            for a in viewer_soup.find_all('a', href=True):
                 if('/assembly' in a['href']):
-                    theURL = temporaryURL + a['href']
+                    theURL = a['href']
                     break
             if theURL:
                 return theURL
-            else:
-                new_request = requests.get("https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id="+accession+"&db=nuccore&extrafeat=0&fmt_mask=0&retmod=html&withmarkup=on&tool=portal&log")
-                soup = bs4.BeautifulSoup(new_request.text)
-                for a in soup.find_all('a', href=True):
-                    if('/assembly' in a['href']):
-                        theURL = a['href']
-                        break
-                if theURL:
-                    return theURL
-                else: 
-                    raise ValueError
-        except ValueError as e:
-            print("RefSeq Accession is invalid")
+            else: 
+                raise ValueError("Invalid accession, no assembly URL")
+    except ValueError as e:
+        print(e)
+
+def initialRequest(accession):
+        global the_request
+        the_request = requests.get("https://www.ncbi.nlm.nih.gov/nuccore/" + accession)
+        global soup
+        soup = bs4.BeautifulSoup(the_request.text)
+        global viewer_nucleotide
+        viewerNucleotide = requests.get("https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id="+accession+"&db=nuccore&extrafeat=0&fmt_mask=0&retmod=html&withmarkup=on&tool=portal&log")
+        global viewer_soup
+        viewer_soup = bs4.BeautifulSoup(viewerNucleotide.text)
+
+## Gets the HTML file for the NCBI accession number of interest.
+def get_accession_data(accession):
+    initialRequest(accession)
 
 ## Gets the URL for the Assembly link from NCBI's refseq accession
-    temporaryURL2 = find_url('/assembly', soup)
+    temporaryURL2 = find_url('/assembly', soup, viewer_soup)
     new_request = requests.get(temporaryURL2)
     new_soup = bs4.BeautifulSoup(new_request.text)
+    
 
 ## Finds the div element with a rprt class
     if not new_soup.find_all('a', attrs={'href': re.compile("^ftp://ftp.ncbi.nlm.nih.gov/genomes/all")}):
         items = new_soup.find("div", class_="rprt")
 
 ## Get's the assembly URL and gets the assembly url HTML file.
-        temporaryURL2 = find_url('/assembly', items)
-
+        temporaryURL2 = find_url('/assembly', items, viewer_soup)
         new_request = requests.get(temporaryURL2)
         new_soup = bs4.BeautifulSoup(new_request.text)
 
@@ -81,5 +99,28 @@ def get_assembly_data(accession):
     subprocess.call(["wget", downloadUrl])
     subprocess.call(["gunzip", "-f", "*.gz"])
     return lastPieceOfUrl + "_cds_from_genomic.fna"
+
+
+def get_assembly_accession(accession):
+    initialRequest(accession)
+    temporaryURL2 = find_url('/assembly', soup, viewer_soup)
+    new_request = requests.get(temporaryURL2)
+    new_soup = bs4.BeautifulSoup(new_request.text)
+    if not new_soup.find_all('a', attrs={'href': re.compile("^ftp://ftp.ncbi.nlm.nih.gov/genomes/all")}):
+        items = new_soup.find("div", class_="rprt")
+        temporaryURL2 = find_url('/assembly', items, viewer_soup)
+        new_request = requests.get(temporaryURL2)
+        new_soup = bs4.BeautifulSoup(new_request.text)
+    assemblyID = temporaryURL2.rsplit('/', 1)[-1]
+    if not assemblyID:
+        raise ValueError("An error has occured at get_assembly_accession")
+    return assemblyID
+    
+  
+
+
+    
+   
+
 
 
--- testApp/app/__init__.py
+++ testApp/app/__init__.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 ## This is a configuration file. UPLOAD_FOLDER specifies where user uploads will go. 
 ## The secret_key is necessary to run and should be hard to guess. 
 ## the MAX_CONTENT_LENGTH can be changed to change how large uploads can be. For example 18 * 1024 * 1024 will allow 18MB uploads. 
-UPLOAD_FOLDER = r"/home/bdehlinger/DCB/DCB-Dynamic-Codon-Biaser/testApp/"
+UPLOAD_FOLDER = r"/home/bdehlinger/DCB-Dynamic-Codon-Biaser/testApp/"
 app = Flask(__name__)
 app.secret_key = "SNiloZjXiAvZEQoO9mvVH5FTEZjInKyX"
 bootstrap = Bootstrap(app)
